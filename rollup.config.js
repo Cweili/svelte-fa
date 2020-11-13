9e570a4942c4d4551c660530743d200789419bee
Cweili <3weili@gmail.com>
2020-11-13 14:43:01 +0800

    fix: rollup warns default export for commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -4,43 +4,46 @@ import common from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import pkg from './package.json';
 
-export default [{
-  input: pkg.svelte,
-  output: [
-    {
-      file: pkg.browser,
-      format: 'iife',
-      name: 'SvelteFa',
-    },
-  ],
-  plugins: [
-    svelte({
-      legacy: true,
-    }),
-    resolve({
-      browser: true,
-    }),
-    common(),
-    babel({
-      extensions: ['.js', '.jsx', '.es6', '.es', '.mjs', '.svelte'],
-    }),
-  ],
-},
-{
-  input: pkg.svelte,
-  output: [
-    {
-      file: pkg.module,
-      format: 'es',
-    },
-    {
-      file: pkg.browser.replace(/\.js$/, '.cjs.js'),
-      format: 'cjs',
-    },
-  ],
-  plugins: [
-    svelte({
-      legacy: true,
-    }),
-  ],
-}];
+export default [
+  {
+    input: pkg.svelte,
+    output: [
+      {
+        file: pkg.browser,
+        format: 'iife',
+        name: 'SvelteFa',
+      },
+    ],
+    plugins: [
+      svelte({
+        legacy: true,
+      }),
+      resolve({
+        browser: true,
+      }),
+      common(),
+      babel({
+        extensions: ['.js', '.jsx', '.es6', '.es', '.mjs', '.svelte'],
+      }),
+    ],
+  },
+  {
+    input: pkg.svelte,
+    output: [
+      {
+        file: pkg.module,
+        format: 'es',
+      },
+      {
+        file: pkg.browser.replace(/\.js$/, '.cjs.js'),
+        format: 'cjs',
+        exports: 'default',
+      },
+    ],
+    plugins: [
+      svelte({
+        legacy: true,
+      }),
+    ],
+  },
+];
