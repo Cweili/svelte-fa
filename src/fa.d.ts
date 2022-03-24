e8385d5439defae2479881fa224033b2951a4c3e
Nayan Gautam <gtmnayan@gmail.com>
2022-03-24 07:27:31 +0000

    Not today prettier

--- src/fa.d.ts
+++ src/fa.d.ts
@@ -12,19 +12,19 @@ declare class Fa extends SvelteComponent {
     icon: IconDefinition
 
     size?:
-      | "lg"
-      | "xs"
-      | "sm"
-      | "1x"
-      | "2x"
-      | "3x"
-      | "4x"
-      | "5x"
-      | "6x"
-      | "7x"
-      | "8x"
-      | "9x"
-      | "10x"
+      | 'lg'
+      | 'xs'
+      | 'sm'
+      | '1x'
+      | '2x'
+      | '3x'
+      | '4x'
+      | '5x'
+      | '6x'
+      | '7x'
+      | '8x'
+      | '9x'
+      | '10x'
       | (`${number}x` & {});
     color?: string;
 
