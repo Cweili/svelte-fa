0e4290f09572d00b6081049709bb0bfdbf6bf783
Cweili <3weili@gmail.com>
2022-06-09 21:03:01 +0800

    docs: support fontawesome 6

--- docs/docs.svelte
+++ docs/docs.svelte
@@ -18,7 +18,7 @@ import {
   faSpinner,
   faStroopwafel,
   faSync,
-  faMagic,
+  faSeedling,
   faTimes,
   faHeart,
   faPlay,
@@ -91,23 +91,23 @@ Gatsby believed in the green light, the orgastic future that year by year recede
 <Fa icon={faStroopwafel} size="3x" spin />`,
   ],
   powerTransforms: [
-    `<Fa icon={faMagic} size="4x" style="background: mistyrose" />
-<Fa icon={faMagic} scale={0.5} size="4x" style="background: mistyrose" />
-<Fa icon={faMagic} scale={1.2} size="4x" style="background: mistyrose" />`,
-    `<Fa icon={faMagic} scale={0.5} size="4x" style="background: mistyrose" />
-<Fa icon={faMagic} scale={0.5} translateX={0.2} size="4x" style="background: mistyrose" />
-<Fa icon={faMagic} scale={0.5} translateX={-0.2} size="4x" style="background: mistyrose" />
-<Fa icon={faMagic} scale={0.5} translateY={0.2} size="4x" style="background: mistyrose" />
-<Fa icon={faMagic} scale={0.5} translateY={-0.2} size="4x" style="background: mistyrose" />`,
-    `<Fa icon={faMagic} size="4x" rotate={90} style="background: mistyrose" />
-<Fa icon={faMagic} size="4x" rotate={180} style="background: mistyrose" />
-<Fa icon={faMagic} size="4x" rotate="270" style="background: mistyrose" />
-<Fa icon={faMagic} size="4x" rotate="30" style="background: mistyrose" />
-<Fa icon={faMagic} size="4x" rotate="-30" style="background: mistyrose" />
-<Fa icon={faMagic} size="4x" flip="vertical" style="background: mistyrose" />
-<Fa icon={faMagic} size="4x" flip="horizontal" style="background: mistyrose" />
-<Fa icon={faMagic} size="4x" flip="both" style="background: mistyrose" />
-<Fa icon={faMagic} size="4x" flip="both" style="background: mistyrose" />`,
+    `<Fa icon={faSeedling} size="4x" style="background: mistyrose" />
+<Fa icon={faSeedling} scale={0.5} size="4x" style="background: mistyrose" />
+<Fa icon={faSeedling} scale={1.2} size="4x" style="background: mistyrose" />`,
+    `<Fa icon={faSeedling} scale={0.5} size="4x" style="background: mistyrose" />
+<Fa icon={faSeedling} scale={0.5} translateX={0.2} size="4x" style="background: mistyrose" />
+<Fa icon={faSeedling} scale={0.5} translateX={-0.2} size="4x" style="background: mistyrose" />
+<Fa icon={faSeedling} scale={0.5} translateY={0.2} size="4x" style="background: mistyrose" />
+<Fa icon={faSeedling} scale={0.5} translateY={-0.2} size="4x" style="background: mistyrose" />`,
+    `<Fa icon={faSeedling} size="4x" rotate={90} style="background: mistyrose" />
+<Fa icon={faSeedling} size="4x" rotate={180} style="background: mistyrose" />
+<Fa icon={faSeedling} size="4x" rotate="270" style="background: mistyrose" />
+<Fa icon={faSeedling} size="4x" rotate="30" style="background: mistyrose" />
+<Fa icon={faSeedling} size="4x" rotate="-30" style="background: mistyrose" />
+<Fa icon={faSeedling} size="4x" flip="vertical" style="background: mistyrose" />
+<Fa icon={faSeedling} size="4x" flip="horizontal" style="background: mistyrose" />
+<Fa icon={faSeedling} size="4x" flip="both" style="background: mistyrose" />
+<Fa icon={faSeedling} size="4x" flip="both" style="background: mistyrose" />`,
   ],
   layering: [
     `import Fa, {
@@ -350,74 +350,74 @@ Gatsby believed in the green light, the orgastic future that year by year recede
   <DocsTitle title="Power Transforms" />
   <DocsTitle title="Scaling" level={3} />
   <div class="shadow-sm p-3 mb-3 rounded">
-    <Fa icon={faMagic} size="4x" style="background: mistyrose" />
-    <Fa icon={faMagic} scale={0.5} size="4x" style="background: mistyrose" />
-    <Fa icon={faMagic} scale={1.2} size="4x" style="background: mistyrose" />
+    <Fa icon={faSeedling} size="4x" style="background: mistyrose" />
+    <Fa icon={faSeedling} scale={0.5} size="4x" style="background: mistyrose" />
+    <Fa icon={faSeedling} scale={1.2} size="4x" style="background: mistyrose" />
   </div>
   <DocsCode code={codes.powerTransforms[0]} />
 
   <DocsTitle title="Positioning" level={3} />
   <div class="shadow-sm p-3 mb-3 rounded">
-    <Fa icon={faMagic} scale={0.5} size="4x" style="background: mistyrose" />
-    <Fa icon={faMagic} scale={0.5} translateX={0.2} size="4x" style="background: mistyrose" />
-    <Fa icon={faMagic} scale={0.5} translateX={-0.2} size="4x" style="background: mistyrose" />
-    <Fa icon={faMagic} scale={0.5} translateY={0.2} size="4x" style="background: mistyrose" />
-    <Fa icon={faMagic} scale={0.5} translateY={-0.2} size="4x" style="background: mistyrose" />
+    <Fa icon={faSeedling} scale={0.5} size="4x" style="background: mistyrose" />
+    <Fa icon={faSeedling} scale={0.5} translateX={0.2} size="4x" style="background: mistyrose" />
+    <Fa icon={faSeedling} scale={0.5} translateX={-0.2} size="4x" style="background: mistyrose" />
+    <Fa icon={faSeedling} scale={0.5} translateY={0.2} size="4x" style="background: mistyrose" />
+    <Fa icon={faSeedling} scale={0.5} translateY={-0.2} size="4x" style="background: mistyrose" />
   </div>
   <DocsCode code={codes.powerTransforms[1]} />
 
   <DocsTitle title="Rotating &amp; Flipping" level={3} />
   <div class="shadow-sm p-3 mb-3 rounded">
     <Fa
-      icon={faMagic}
+      icon={faSeedling}
       rotate={90}
       size="4x"
       style="background: mistyrose"
     />
     <Fa
-      icon={faMagic}
+      icon={faSeedling}
       rotate={180}
       size="4x"
       style="background: mistyrose"
     />
     <Fa
-      icon={faMagic}
+      icon={faSeedling}
       size="4x"
       rotate="270"
       style="background: mistyrose"
     />
     <Fa
-      icon={faMagic}
+      icon={faSeedling}
       size="4x"
       rotate="30"
       style="background: mistyrose"
     />
     <Fa
-      icon={faMagic}
+      icon={faSeedling}
       size="4x"
       rotate="-30"
       style="background: mistyrose"
     />
     <Fa
-      icon={faMagic}
+      icon={faSeedling}
       size="4x"
       flip="vertical"
       style="background: mistyrose"
     />
     <Fa
-      icon={faMagic}
+      icon={faSeedling}
       size="4x"
       flip="horizontal"
       style="background: mistyrose"
     />
     <Fa
-      icon={faMagic}
+      icon={faSeedling}
       size="4x"
       flip="both"
       style="background: mistyrose"
     />
     <Fa
-      icon={faMagic}
+      icon={faSeedling}
       size="4x"
       flip="both"
       rotate="30"
