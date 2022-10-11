c4366781be2e2352e3a8d77892cc3cf7101ec8ac
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2022-10-11 22:55:57 +0100

    feat(docs/components): create `sections` folder and create a file for each section of documentation

---
+++ docs/components/sections/animating-icons.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Fa from "svelte-fa";
+  import {
+    faCircleNotch,
+    faCog,
+    faSpinner,
+    faStroopwafel,
+    faSync,
+  } from "@fortawesome/free-solid-svg-icons";
+
+  import DocsCode from "../ui/docs-code.svelte";
+  import DocsTitle from "../ui/docs-title.svelte";
+
+  import codes from "../../content/codes";
+</script>
+
+<DocsTitle title="Animating Icons" />
+<div class="shadow-sm p-3 mb-3 rounded">
+  <Fa icon={faSpinner} size="3x" spin />
+  <Fa icon={faCircleNotch} size="3x" spin />
+  <Fa icon={faSync} size="3x" spin />
+  <Fa icon={faCog} size="3x" spin />
+  <Fa icon={faSpinner} size="3x" pulse />
+  <Fa icon={faStroopwafel} size="3x" spin />
+</div>
+<DocsCode code={codes.animatingIcons[0]} />
---
+++ docs/components/sections/installation.svelte
@@ -0,0 +1,34 @@
+<script>
+  import DocsCode from "../ui/docs-code.svelte";
+  import DocsTitle from "../ui/docs-title.svelte";
+
+  import codes from "../../content/codes";
+</script>
+
+<DocsTitle title="Installation" />
+<DocsCode code={codes.installation[0]} />
+<div class="shadow-sm p-3 mb-3 rounded clearfix">
+  Install FontAwesome icons via <a
+    href="https://www.npmjs.com/search?q=%40fortawesome%20svg%20icons"
+    target="_blank">official packages</a
+  >, for example:
+</div>
+<DocsCode code={codes.installation[1]} />
+<div class="shadow-sm p-3 mb-3 rounded clearfix">
+  <strong
+    >Notice for <a href="https://sapper.svelte.dev/" target="_blank">Sapper</a> user:</strong
+  > You may need to install the component as a devDependency:
+</div>
+<DocsCode code={codes.installation[2]} />
+<div class="shadow-sm p-3 mb-3 rounded clearfix">
+  <strong
+    >Notice for <a href="https://kit.svelte.dev/" target="_blank">SvelteKit</a
+    >/<a href="https://www.npmjs.com/package/vite" target="_blank">Vite</a> user:</strong
+  > You may need to import the component explicitly as below:
+</div>
+<DocsCode code={codes.installation[3]} lang="js" />
+<div class="shadow-sm p-3 mb-3 rounded clearfix">
+  When using typescript with SvelteKit/Vite, you may also needed to add type
+  definitions that redirect to the non-index.es export:
+</div>
+<DocsCode code={codes.installation[4]} lang="js" />
