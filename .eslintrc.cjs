f099c912de6d5741bbc0fae019196a6313de3325
Marek Dědič <developer@dedic.eu>
2024-01-15 12:12:32 +0100

    Enabled the svelte/no-inline-styles lint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,6 +26,9 @@ module.exports = {
       parserOptions: {
         parser: "@typescript-eslint/parser",
       },
+      rules: {
+        "svelte/no-inline-styles": "error",
+      },
     },
   ],
 };
